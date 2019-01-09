76276d90c79c736c78fccd86732012e3fe41d9d8
uuinnk <uuinnk@developmentontheedge.com>
2019-01-09 12:43:44 +0700

    fixes

--- src/main/java/ru/biosoft/muscle/MuscleServletConfig.java
+++ src/main/java/ru/biosoft/muscle/MuscleServletConfig.java
@@ -2,18 +2,18 @@ package ru.biosoft.muscle;
 
 import com.developmentontheedge.be5.modules.core.CoreModule;
 import com.developmentontheedge.be5.server.ScopedServerModule;
-import com.developmentontheedge.be5.server.servlet.Be5GuiceServletContextListener;
+import com.developmentontheedge.be5.server.servlet.Be5ServletListener;
 import com.developmentontheedge.be5.server.servlet.TemplateModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 
-public class MuscleServletConfig extends Be5GuiceServletContextListener
+public class MuscleServletConfig extends Be5ServletListener
 {
     @Override
     protected Injector getInjector()
     {
-        return Guice.createInjector(
+        return Guice.createInjector(getStage(),
                 new CoreModule(),
                 new ScopedServerModule(),
                 new TemplateModule(),
