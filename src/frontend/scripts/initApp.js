79c25533aa4ae766409de513a0dfc886e6490bb6
uuinnk <uuinnk@developmentontheedge.com>
2019-02-11 12:03:43 +0700

    update be5 api be5init.init() -> initBe5App()

--- src/frontend/scripts/initApp.js
+++ src/frontend/scripts/initApp.js
@@ -1,17 +1,30 @@
 import ReactDOM     from 'react-dom';
 import React        from 'react';
 import { Provider } from 'react-redux';
-import {Application, be5init, createBaseStore, rootReducer} from 'be5-react';
+import {Application, initBe5App, createBaseStore, rootReducer} from 'be5-react';
 import App from './components/Application'
 import './register';
 
 
 const store = createBaseStore(rootReducer);
-be5init.init(store);
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    </AppContainer>,
+    document.getElementById('app'),
+  )
+};
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
-  document.getElementById('app')
-);
+initBe5App(store, function () {
+  render(App);
+});
+
+//Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./components/Application', () => {
+    render(Application)
+  })
+}
