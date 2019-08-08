ad1c3e0c7c04bee0ef58e8aabeeb95ad2e9d5993
Fedor <fedor@DevelopmentOnTheEdge.com>
2019-08-08 11:04:50 +0700

    new Reade.md, MucleMain, local_10 profile for PostgeSQL 10

---
+++ src/main/java/ru/biosoft/muscle/MuscleMain.java
@@ -0,0 +1,11 @@
+package ru.biosoft.muscle;
+
+import com.developmentontheedge.be5.jetty.EmbeddedJetty;
+
+public class MuscleMain
+{
+    public static void main(String... args)
+    {
+        new EmbeddedJetty().run();
+    }
+}
